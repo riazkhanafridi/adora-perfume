09dd61c3c9d958c06230374a2463a9f92fd4fa9a
riaz afridi <riazkhanafridi96@gmail.com>
2024-09-20 22:30:18 +0500

    installation of adora-perfume project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant={"default"} size={"lg"}>
+        click me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
